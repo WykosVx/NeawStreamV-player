3ce7b07e45189412ef6d804dce164ebf5085c67d
William Duarte <bosterwilliam23@gmail.com>
2026-06-19 15:28:24 -0300

    Implement dynamic theme color using ValueNotifier
    Refactor MyApp to use ValueListenableBuilder for dynamic theme color.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
-import 'screens/home_screen.dart';
+import 'screens/home_screen.dart'; 
+import 'app_settings.dart'; 
+
+final ValueNotifier<Color> globalAccentColor = ValueNotifier(Colors.blue);
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
   MediaKit.ensureInitialized();
+  
+  final settings = await AppSettings.getSettings();
+  final int savedColorValue = settings['themeColor'] ?? Colors.blue.value;
+  globalAccentColor.value = Color(savedColorValue);
+  
   runApp(const MyApp());
 }
 
@@ -13,11 +22,23 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Neaw Stream',
-      theme: ThemeData.dark(),
-      home: const HomeScreen(),
+    return ValueListenableBuilder<Color>(
+      valueListenable: globalAccentColor,
+      builder: (context, accentColor, _) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Neaw Stream',
+          theme: ThemeData(
+            brightness: Brightness.dark,
+            colorScheme: ColorScheme.fromSeed(
+              seedColor: accentColor,
+              brightness: Brightness.dark,
+            ),
+            useMaterial3: true,
+          ),
+          home: const HomeScreen(),
+        );
+      },
     );
   }
 }
