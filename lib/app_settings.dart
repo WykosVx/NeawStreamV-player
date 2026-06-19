47ab7b9f2a430b617f85414630d42b63f67f40c4
William Duarte <bosterwilliam23@gmail.com>
2026-06-19 15:29:08 -0300

    Update app_settings.dart

--- lib/app_settings.dart
+++ lib/app_settings.dart
@@ -1,23 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AppSettings {
   static const String keyBufferSize = 'buffer_size';
-  static const String keyUserAgent = 'user_agent';
   static const String keyShowLogs = 'auto_mostrar_logs';
+  static const String keyThemeColor = 'theme_color';
 
-  static Future<void> saveSettings(int buffer, String ua, bool showLogs) async {
+  static Future<void> saveSettings(int buffer, bool showLogs, int colorValue) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setInt(keyBufferSize, buffer);
-    await prefs.setString(keyUserAgent, ua);
-    await prefs.setBool(keyShowLogs, showLogs); 
+    await prefs.setBool(keyShowLogs, showLogs);
+    await prefs.setInt(keyThemeColor, colorValue);
   }
-
   static Future<Map<String, dynamic>> getSettings() async {
     final prefs = await SharedPreferences.getInstance();
+    
     return {
       'buffer': prefs.getInt(keyBufferSize) ?? 128,
-      'ua': prefs.getString(keyUserAgent) ?? 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36',
       'showLogs': prefs.getBool(keyShowLogs) ?? false, 
+      'themeColor': prefs.getInt(keyThemeColor) ?? Colors.blue.value,
     };
   }
 }
