4a60a884c11328c06af01736dcb086ebc2ca30a4
William Duarte <bosterwilliam23@gmail.com>
2026-06-20 14:25:01 -0300

    Refactor build.gradle.kts to simplify build directory setup
    Removed unnecessary allprojects block and simplified build directory assignment.

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,14 +1,4 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
+val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
