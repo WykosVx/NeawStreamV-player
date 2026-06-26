2a47b40bfad18ac5b80450804f57dc22fd8a4bb3
WykosVx <bosterwilliam23@gmail.com>
2026-06-25 22:14:31 -0300

    Integrate Sentry for error and performance tracking
    Initialize Sentry for error tracking and performance monitoring in the app.

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:media_kit/media_kit.dart';
 import 'screens/home_screen.dart';
 import 'app_settings.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
+
 
 final ValueNotifier<Color> globalAccentColor = ValueNotifier(Colors.blue);
 
@@ -11,7 +13,14 @@ void main() async {
 
   await _loadInitialSettings();
 
-  runApp(const NeawApp());
+  await SentryFlutter.init(
+    (options) {
+      options.dsn = 'https://1e07e7afaf5eb3921c2dfb8c363c747b@o4511626533208064.ingest.us.sentry.io/4511626540941312';
+      options.tracesSampleRate = 1.0;
+      options.profilesSampleRate = 1.0;
+    },
+    appRunner: () => runApp(SentryWidget(child: const NeawApp())),
+  );
 }
 
 Future<void> _loadInitialSettings() async {
