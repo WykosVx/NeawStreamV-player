d9e6b558d7ddc3131974b4e0de27742ecdedd5ff
WykosVx <bosterwilliam23@gmail.com>
2026-08-19 21:54:38 -0300

    Clean up main.dart by removing MediaKit
    Removed unused imports and Sentry initialization.

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:media_kit/media_kit.dart';
 import 'screens/home_screen.dart';
 import 'app_settings.dart';
-import 'package:sentry_flutter/sentry_flutter.dart';
-
 
 final ValueNotifier<Color> globalAccentColor = ValueNotifier(Colors.blue);
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  MediaKit.ensureInitialized();
-
   await _loadInitialSettings();
-
-  await SentryFlutter.init(
-    (options) {
-      options.dsn = 'https://1e07e7afaf5eb3921c2dfb8c363c747b@o4511626533208064.ingest.us.sentry.io/4511626540941312';
-      options.tracesSampleRate = 1.0;
-      options.profilesSampleRate = 1.0;
-    },
-    appRunner: () => runApp(SentryWidget(child: const NeawApp())),
-  );
+  runApp(const NeawApp());
 }
 
 Future<void> _loadInitialSettings() async {
